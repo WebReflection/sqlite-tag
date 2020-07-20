3367d6132eb3e46841bae25370fe4ac9ce53d180
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-07-20 13:43:17 +0200

    using better helpers

--- cjs/index.js
+++ cjs/index.js
@@ -1,42 +1,16 @@
 'use strict';
-const {defineProperty} = Object;
-class Raw {
-  constructor(value) {
-    defineProperty(this, 'sql', {value});
-  }
-}
+const plain = (m => m.__esModule ? /* istanbul ignore next */ m.default : /* istanbul ignore next */ m)(require('plain-tag'));
+const {asStatic, asParams} = require('static-params');
 
 const create = (db, name) => (tpl, ...values) => new Promise((res, rej) => {
-  const {sql, params} = ditchRaws(tpl, values);
+  const [sql, ...params] = asParams(tpl, ...values);
   db[name](sql.join('?'), params, (err, val) => {
-    if (err)
-      rej(err);
-    else
-      res(val);
+    if (err) rej(err);
+    else res(val);
   });
 });
 
-const ditchRaws = (tpl, values) => {
-  const sql = [tpl[0]];
-  const params = [];
-  for (let i = 1, j = 0, {length} = tpl; i < length; i++) {
-    const value = values[i - 1];
-    if (value instanceof Raw)
-      sql[j] += value.sql + tpl[i];
-    else {
-      j = params.push(value);
-      sql.push(tpl[i]);
-    }
-  }
-  return {sql, params};
-};
-
-const raw = (tpl, ...values) => {
-  const out = [tpl[0]];
-  for (let i = 1, {length} = tpl; i < length; i++)
-    out.push(values[i - 1], tpl[i]);
-  return new Raw(out.join(''));
-};
+const raw = (tpl, ...values) => asStatic(plain(tpl, ...values));
 
 function SQLiteTag(db) {
   return {
