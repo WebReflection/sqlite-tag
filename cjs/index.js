b35eaf0c41c5c21bd0b3e7f5838a47a4c0936f2b
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-11-16 11:22:52 +0100

    Fixed return on invalid values

--- cjs/index.js
+++ cjs/index.js
@@ -1,29 +1,30 @@
 'use strict';
-const plain = (m => m.__esModule ? /* istanbul ignore next */ m.default : /* istanbul ignore next */ m)(require('plain-tag'));
+const plain = (m => /* c8 ignore start */ m.__esModule ? m.default : m /* c8 ignore stop */)(require('plain-tag'));
 const {asStatic, asParams} = require('static-params');
 
 const create = (db, name) => (tpl, ...values) => new Promise((res, rej) => {
-  if (tpl.some(invalid))
-    rej(error(new Error('SQLITE_ERROR: SQL injection hazard')));
-  const [sql, ...params] = asParams(tpl, ...values);
-  db[name](sql.join('?'), params, (err, val) => {
-    if (err) rej(err);
-    else res(val);
-  });
+  if (tpl.some(chunk => chunk.includes('?'))) {
+    const error = new Error('SQLITE_ERROR: SQL injection hazard');
+    error.code = 'SQLITE_ERROR';
+    rej(error);
+  }
+  else {
+    const [sql, ...params] = asParams(tpl, ...values);
+    db[name](sql.join('?'), params, (err, val) => {
+      if (err)
+        rej(err);
+      else
+        res(val);
+    });
+  }
 });
-
-const error = error => ((error.code = 'SQLITE_ERROR'), error);
-
-const raw = (tpl, ...values) => asStatic(plain(tpl, ...values));
-
-const invalid = chunk => chunk.includes('?');
 
 function SQLiteTag(db) {
   return {
     all: create(db, 'all'),
     get: create(db, 'get'),
     query: create(db, 'run'),
-    raw
+    raw: (tpl, ...values) => asStatic(plain(tpl, ...values))
   };
 }
 module.exports = SQLiteTag;
--- esm/index.js
+++ esm/index.js
@@ -2,26 +2,27 @@ import plain from 'plain-tag';
 import {asStatic, asParams} from 'static-params';
 
 const create = (db, name) => (tpl, ...values) => new Promise((res, rej) => {
-  if (tpl.some(invalid))
-    rej(error(new Error('SQLITE_ERROR: SQL injection hazard')));
-  const [sql, ...params] = asParams(tpl, ...values);
-  db[name](sql.join('?'), params, (err, val) => {
-    if (err) rej(err);
-    else res(val);
-  });
+  if (tpl.some(chunk => chunk.includes('?'))) {
+    const error = new Error('SQLITE_ERROR: SQL injection hazard');
+    error.code = 'SQLITE_ERROR';
+    rej(error);
+  }
+  else {
+    const [sql, ...params] = asParams(tpl, ...values);
+    db[name](sql.join('?'), params, (err, val) => {
+      if (err)
+        rej(err);
+      else
+        res(val);
+    });
+  }
 });
-
-const error = error => ((error.code = 'SQLITE_ERROR'), error);
-
-const raw = (tpl, ...values) => asStatic(plain(tpl, ...values));
-
-const invalid = chunk => chunk.includes('?');
 
 export default function SQLiteTag(db) {
   return {
     all: create(db, 'all'),
     get: create(db, 'get'),
     query: create(db, 'run'),
-    raw
+    raw: (tpl, ...values) => asStatic(plain(tpl, ...values))
   };
 };
